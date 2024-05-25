fd6419e2ba70a167544874f98a75e21539d72e11
Lilia Levchenko <liliya.benatti@gmail.com>
2024-05-25 23:08:37 +0200

    upd count

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,6 +1,5 @@
 import { PATH_DB } from '../constants/contacts.js';
 import fs from 'fs/promises';
-import { createFakeContact } from '../utils/createFakeContact.js';
 
 export const getAllContacts = async () => {
   try {
